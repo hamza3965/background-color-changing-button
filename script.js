8ceb6513bdf3b2916f3c767490cbcf64a9f04f3c
Hamza <blitstyle@gmail.com>
2024-09-08 11:12:01 +0500

    btn-completed

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+function changeBackgroundColor() {
+    const container = document.getElementById('background-container');
+    
+    // Set the background color property
+    container.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    // Generate a random hex color code
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
